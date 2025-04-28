83cf261d9cd89fb83035577c80558ae432da25df
nguyentra04 <nguyentra8704@gmail.com>
2025-04-28 21:40:23 +0700

    Day01

---
+++ MyAppNHT/MyAppNHT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyAppNHT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
